081c0d99f38681ce1bd0be08b4f1f65cd8d0b117
alan <aafatikhov@gmail.com>
2024-11-03 01:24:54 +0500

    added models for hotels and users

--- opentrip/hotels/models.py
+++ opentrip/hotels/models.py
@@ -1,3 +1,151 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.conf import settings
 
-# Create your models here.
+
+def hotel_preview_directory_path(instance, filename):
+    return f'hotels/{instance.name}/preview_{filename}'
+
+def hotel_photo_directory_path(instance, filename):
+    return f'hotels/{instance.name}/{filename}'
+
+def room_preview_directory_path(instance, filename):
+    return f'rooms/{instance.number}/preview_{filename}'
+
+def room_photo_directory_path(instance, filename):
+    return f'rooms/{instance.number}/{filename}'
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=70, verbose_name='Название отеля')
+    address = models.CharField(max_length=80, verbose_name='Адрес отеля')
+    city = models.CharField(max_length=70, verbose_name='Город')
+    country = models.CharField(max_length=70, verbose_name='Страна')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание отеля')
+    rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(5.0)], verbose_name='Рейтинг отеля')
+    preview = models.ImageField(null=True, blank=True,
+                                upload_to=hotel_preview_directory_path,
+                                verbose_name='Главное фото отеля')
+    images = models.ManyToManyField(
+        "HotelImage",
+        verbose_name='Фотографии отеля',
+        related_name='hotels',
+        blank=True
+    )
+    amenities = models.ManyToManyField(
+        "Amenity",
+        verbose_name='Удобства отеля',
+        related_name='hotels',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Отель'
+        verbose_name_plural = 'Отели'
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(models.Model):
+    hotel = models.ForeignKey(Hotel, verbose_name='Отель', related_name='room_types', on_delete=models.CASCADE)
+    name = models.CharField(max_length=60, verbose_name='Название типа номера')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание типа номера')
+    occupancy = models.PositiveIntegerField(verbose_name='Максимальное количество гостей')
+    amenities = models.ManyToManyField("Amenity", verbose_name='Удобства типа номера', related_name='room_types', blank=True)
+
+    class Meta:
+        verbose_name = 'Тип номера'
+        verbose_name_plural = 'Типы номеров'
+        unique_together = ['hotel', 'name']
+    def __str__(self):
+        return f"{self.hotel.name} - {self.name}"
+
+
+
+class Room(models.Model):
+    room_type = models.ForeignKey(RoomType, verbose_name='Тип номера', related_name='rooms', on_delete=models.CASCADE)
+    number = models.CharField(max_length=10, verbose_name='Номер комнаты')
+    preview = models.ImageField(null=True, blank=True,
+                                upload_to=room_preview_directory_path,
+                                verbose_name='Главное фото номера')
+    images = models.ManyToManyField(
+        "RoomImage",
+        verbose_name='Фотографии номера',
+        related_name='rooms',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Номер'
+        verbose_name_plural = 'Номера'
+        unique_together = ['room_type', 'number']
+
+
+    def __str__(self):
+        return f"{self.room_type.hotel.name} - {self.room_type.name} - {self.number}"
+
+
+class Amenity(models.Model):
+    name = models.CharField(max_length=80, verbose_name='Название удобства', unique=True)
+
+    class Meta:
+        verbose_name = 'Удобство'
+        verbose_name_plural = 'Удобства'
+
+    def __str__(self):
+        return self.name
+
+
+class HotelImage(models.Model):
+    hotel = models.ForeignKey(
+        Hotel,
+        verbose_name='Отель',
+        related_name='hotel_images',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to=hotel_photo_directory_path, verbose_name='Фотография отеля')
+
+    class Meta:
+        verbose_name = 'Фотография отеля'
+        verbose_name_plural = 'Фотографии отеля'
+
+    def __str__(self):
+        return f"Фотография для {self.hotel.name}"
+
+
+class RoomImage(models.Model):
+    room = models.ForeignKey(
+        Room,
+        verbose_name='Номер',
+        related_name='room_images',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to=room_photo_directory_path, verbose_name='Фотография номера')
+
+    class Meta:
+        verbose_name = 'Фотография номера'
+        verbose_name_plural = 'Фотографии номера'
+
+    def __str__(self):
+        return f"Фотография для номера {self.room.number}"
+
+
+class Review(models.Model):
+    hotel = models.ForeignKey(Hotel, verbose_name='Отель', related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name='Рейтинг')
+    comment = models.TextField(verbose_name='Комментарий', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        unique_together = ['hotel', 'user']
+
+    def __str__(self):
+        return f"Отзыв от {self.user.username} на {self.hotel.name}"
--- opentrip/users/models.py
+++ opentrip/users/models.py
@@ -1,3 +1,114 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from hotels.models import Hotel
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+def user_avatar_directory_path(instance, filename):
+    return f'users/avatars/{instance.user.username}/{filename}'
+
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('customer', 'Обычный пользователь'),
+        ('hotel', 'Отель'),
+    )
+
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default='customer',
+        verbose_name='Тип пользователя'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class HotelProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='hotel_profile',
+        limit_choices_to={'user_type': 'hotel'},
+        verbose_name='Пользователь (отель)'
+    )
+    hotel = models.OneToOneField(
+        Hotel,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name='Отель',
+        related_name='hotel_profile'
+    )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Номер телефона должен быть в формате: '+999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name='Номер телефона')
+    website = models.URLField(blank=True, verbose_name='Веб-сайт')
+
+    class Meta:
+        verbose_name = 'Профиль отеля'
+        verbose_name_plural = 'Профили отелей'
+
+    def __str__(self):
+        return self.user.username if not self.hotel else self.hotel.name
+
+    def save(self, *args, **kwargs):
+        if self._state.adding and self.hotel:
+            self.user.username = self.hotel.name
+            self.user.save()
+        super().save(*args, **kwargs)
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='customer_profile',
+        limit_choices_to={'user_type': 'customer'},
+        verbose_name='Пользователь (клиент)'
+    )
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    surname = models.CharField(max_length=50, verbose_name='Фамилия')
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Номер телефона должен быть в формате: '+999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name='Номер телефона')
+    address = models.CharField(max_length=255, blank=True, verbose_name='Адрес')
+    birthdate = models.DateField(blank=True, null=True, verbose_name="Дата рождения")
+    avatar = models.ImageField(upload_to=user_avatar_directory_path, blank=True, null=True, verbose_name='Аватар пользователя')
+
+
+    class Meta:
+        verbose_name = 'Профиль клиента'
+        verbose_name_plural = 'Профили клиентов'
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.user.first_name = self.name
+            self.user.last_name = self.surname
+            self.user.save()
+        else:
+            orig = CustomerProfile.objects.get(pk=self.pk)
+            if orig.name != self.name:
+                self.user.first_name = self.name
+            if orig.surname != self.surname:
+                self.user.last_name = self.surname
+            self.user.save()
+
+        super().save(*args, **kwargs)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 'hotel':
+            HotelProfile.objects.create(user=instance)
+        elif instance.user_type == 'customer':
+            CustomerProfile.objects.create(user=instance)
